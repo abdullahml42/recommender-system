196150317aa89ec94ec5ebfd665d23c26ef0e5d2
abdullah <140593567+abdullahml42@users.noreply.github.com>
2023-09-04 07:26:59 +0600

    Add requirements and project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="recommender_system",
+    version="0.0.0",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    description="Industrial and Scientific Product Recommender System."
+)
